0ab70bff2564a4b41375881629ff19499cc08dac
qq_32523587 <qq_32523587@noreply.gitcode.com>
2026-09-07 15:47:02 +0800

    Initial commit

---
+++ src/main/java/org/infi/infiaicodemother/InfiAiCodeMotherApplication.java
@@ -0,0 +1,13 @@
+package org.infi.infiaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfiAiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfiAiCodeMotherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/infi/infiaicodemother/InfiAiCodeMotherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.infi.infiaicodemother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfiAiCodeMotherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
