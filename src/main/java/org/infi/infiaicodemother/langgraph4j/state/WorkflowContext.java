20a74093cd8d99af301339c2d4665eab273c44a9
qq_32523587 <qq_32523587@noreply.gitcode.com>
2026-09-16 15:39:20 +0800

    feat(langgraph4j): 添加并发图片收集工作流支持

--- src/main/java/org/infi/infiaicodemother/langgraph4j/state/WorkflowContext.java
+++ src/main/java/org/infi/infiaicodemother/langgraph4j/state/WorkflowContext.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bsc.langgraph4j.prebuilt.MessagesState;
+import org.infi.infiaicodemother.langgraph4j.model.ImageCollectionPlan;
 import org.infi.infiaicodemother.langgraph4j.model.ImageResource;
 import org.infi.infiaicodemother.langgraph4j.model.QualityResult;
 import org.infi.infiaicodemother.model.enums.CodeGenTypeEnum;
@@ -77,6 +78,21 @@ public class WorkflowContext implements Serializable {
      * 质量检查结果
      */
     private QualityResult qualityResult;
+
+
+    /**
+     * 图片收集计划
+     */
+    private ImageCollectionPlan imageCollectionPlan;
+
+
+    /**
+     * 并发图片收集的中间结果字段
+     */
+    private List<ImageResource> contentImages;
+    private List<ImageResource> illustrations;
+    private List<ImageResource> diagrams;
+    private List<ImageResource> logos;
 
 
     @Serial
